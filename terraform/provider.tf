9626f00e8c5c75ec2fd567e4a41e1da27f9f2203
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-04-23 20:13:56 -0600

    remove project name from provider
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,3 @@ provider "teleport" {
   join_token = "hcp-terraform"
   audience_tag = "teleport"
 }
-
-resource "null_resource" "print_token" {
-  provisioner "local-exec" {
-    command = "echo TFC_WORKLOAD_IDENTITY_TOKEN_TELEPORT: $TFC_WORKLOAD_IDENTITY_TOKEN_TELEPORT"
-  }
-}
