66e64f0ea8e17b12d7a3d117a9bde48fc719d665
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-06-16 13:52:43 -0600

    add temp azure devops creds for demo
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

---
+++ terraform/azure_devops.tf
@@ -0,0 +1,41 @@
+resource teleport_role "mwi_demo_azure_devops" {
+  version = "v7"
+  metadata = {
+    name = "mwi-demo-azure-devops"
+  }
+  
+  spec = {
+    allow = {
+      nodeLabels = {
+        "env" = ["mwi-demo"]
+      },
+      logins = ["ubuntu"]
+    }
+  }
+}
+
+resource teleport_bot "mwi_demo_azure_devops" {
+  name = "mwi-demo-azure-devops"
+  roles = [
+    teleport_role.mwi_demo_azure_devops.metadata.name
+  ]
+}
+
+resource "teleport_provision_token" "mwi_demo_azure_devops" {
+  version = "v2"
+  metadata = {
+    name = "mwi-demo-azure-devops"
+  }
+  spec = {
+    roles       = ["Bot"]
+    bot_name    = "mwi-demo-azure-devops"
+    join_method = "azure_devops"
+    azure_devops = {
+      organization_id = "0ca3ddd9-f0b0-4635-a98c-5866526961b6"
+      allow = [{
+        project_name = "testing-azure-devops-join"
+        pipeline_name = "strideynet.azure-devops-testingv"
+      }]
+    }
+  }
+}
