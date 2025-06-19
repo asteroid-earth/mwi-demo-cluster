81d07f4c6aef2f874b173470e4c2a06c01269e84
Noah Stride <noah.stride@goteleport.com>
2025-06-19 16:55:07 +0100

    Fix repository in allow rules again

--- terraform/gha_ssh.tf
+++ terraform/gha_ssh.tf
@@ -34,7 +34,7 @@ resource teleport_provision_token "mwi_demo_gha_ssh" {
     github = {
       enterprise_slug = "teleport"
       allow = [{
-        repository = "asteroid-earth/mwi-demo-cluster"
+        repository = "asteroid-earth/mwi-demo-gha-ssh"
         ref_type   = "branch"
         ref_name   = "main"
       }]
