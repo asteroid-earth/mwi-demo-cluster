10fce99e8c6bcc93dffffee053606377119783a2
Noah Stride <noah.stride@goteleport.com>
2025-06-25 12:50:52 +0100

    More comments on our GHA demo

--- terraform/gha_ssh.tf
+++ terraform/gha_ssh.tf
@@ -1,20 +1,5 @@
-
-resource teleport_role "mwi_demo_gha_ssh" {
-  version = "v7"
-  metadata = {
-    name = "mwi-demo-gha-ssh"
-  }
-
-  spec = {
-    allow = {
-      node_labels = {
-        "env" = ["mwi-demo"]
-      }
-      logins = ["ubuntu"]
-    }
-  }
-}
-
+// This Bot identity will be used to represent our CI/CD. We'll assign it
+// roles to control what it can access.
 resource teleport_bot "mwi_demo_gha_ssh" {
   name = "mwi-demo-gha-ssh"
   roles = [
@@ -44,6 +29,25 @@ resource teleport_provision_token "mwi_demo_gha_ssh" {
         ref_type   = "branch"
         ref_name   = "main"
       }]
+    }
+  }
+}
+
+// This role controls what our Bot is allowed to access. In this case, we'll
+// grant it access to SSH as the `ubuntu` user to any SSH node with the
+// `env=mwi-demo` label.
+resource teleport_role "mwi_demo_gha_ssh" {
+  version = "v7"
+  metadata = {
+    name = "mwi-demo-gha-ssh"
+  }
+
+  spec = {
+    allow = {
+      node_labels = {
+        "env" = ["mwi-demo"]
+      }
+      logins = ["ubuntu"]
     }
   }
 }
