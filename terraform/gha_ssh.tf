9cc434ca3f870eb46569e9163ab238bf2d44a9d4
Noah Stride <noah.stride@goteleport.com>
2025-06-25 12:47:18 +0100

    Add some explantory comments to GHA SSH demo

--- terraform/gha_ssh.tf
+++ terraform/gha_ssh.tf
@@ -22,6 +22,8 @@ resource teleport_bot "mwi_demo_gha_ssh" {
   ]
 }
 
+// This Join Token is used to configure which machines can authenticate as our
+// mwi-demo-gha-ssh bot.
 resource teleport_provision_token "mwi_demo_gha_ssh" {
   version = "v2"
   metadata = {
@@ -30,9 +32,13 @@ resource teleport_provision_token "mwi_demo_gha_ssh" {
   spec = {
     roles       = ["Bot"]
     bot_name    = "mwi-demo-gha-ssh"
+    // Configured to use the GitHub join method - which allows authentication
+    // using the OIDC ID Token issued by GitHub Actions.
     join_method = "github"
     github = {
       enterprise_slug = "teleport"
+      // Only grant access to GitHub Actions running against the main branch
+      // of our specific repository.
       allow = [{
         repository = "asteroid-earth/mwi-demo-gha-ssh"
         ref_type   = "branch"
